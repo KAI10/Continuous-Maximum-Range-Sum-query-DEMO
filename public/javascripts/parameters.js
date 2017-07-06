af34fcae9f49533829f21501202e8200aa938fdd
KAI10 <ikaziashik@gmail.com>
2017-07-06 11:04:44 +0600

    solutions shown in R instead of MBR

---
+++ public/javascripts/parameters.js
@@ -0,0 +1,7 @@
+var R = {d1: 0.01, d2: 0.01};
+var F = {};
+var T = 0;
+
+module.exports.R = R;
+module.exports.F = F;
+module.exports.T = T;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var data = require('../3984.json');
+var parameters = require('../public/javascripts/parameters')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
   	title: 'Express',
-  	result: data
+  	result: data,
+  	parameters: parameters
   });
 });
 
