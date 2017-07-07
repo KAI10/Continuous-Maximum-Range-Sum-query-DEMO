521021547dc27562b4714f579482600fc8068926
KAI10 <ikaziashik@gmail.com>
2017-07-07 17:04:56 +0600

    api key hidden

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var result = require('../public/javascripts/3984.json');
 var data = require('../public/javascripts/3984_locations.json');
+var config = require('../public/javascripts/config.js');
 //var parameters = require('../public/javascripts/parameters')
 
 /* GET home page. */
@@ -9,7 +10,8 @@ router.get('/', function(req, res, next) {
   res.render('index', {
   	title: 'COMAXRS-DEMO',
   	result: result,
-  	data: data
+  	data: data,
+  	api_url: config.api_url
   });
 });
 
